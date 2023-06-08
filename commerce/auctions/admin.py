723772111de9af7a8c69df1b7d5a7350dcfb89fb
Beshoy-Hezky <hezkyb@mcmaster.ca>
2023-06-08 16:53:27 -0400

    F01,F06: Add an auction listing model and add it to super admin user

--- commerce/auctions/admin.py
+++ commerce/auctions/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import User,Category,AuctionListing
 
 # Register your models here.
+admin.site.register(AuctionListing)
+admin.site.register(User)
+admin.site.register(Category)
--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -2,5 +2,18 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+
 class User(AbstractUser):
     pass
+
+
+class AuctionListing(models.Model):
+    header = models.CharField(max_length=64)
+    seller = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name="seller")
+    image_url = models.CharField(max_length=5000)
+    description = models.CharField(max_length=500)
+    is_live = models.BooleanField(default=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True, null=True, related_name="category")
