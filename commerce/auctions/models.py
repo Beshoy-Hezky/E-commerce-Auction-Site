56ae40a2616705a186a618b0b939912ae6cee03c
Beshoy-Hezky <hezkyb@mcmaster.ca>
2023-06-09 09:08:17 -0400

    Reconfigure/fix gitignore problems

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -5,6 +5,8 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
 
 class User(AbstractUser):
     pass
@@ -15,5 +17,9 @@ class AuctionListing(models.Model):
     seller = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name="seller")
     image_url = models.CharField(max_length=5000)
     description = models.CharField(max_length=500)
+    starting_price = models.FloatField(null=True, blank=True, default=0.0)
     is_live = models.BooleanField(default=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True, null=True, related_name="category")
+
+    def __str__(self):
+       return f"{self.header} ({self.category})"
