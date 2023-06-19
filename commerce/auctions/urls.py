dbce4b03426ef16ff8b32958094d1f6781b93f75
Beshoy-Hezky <hezkyb@mcmaster.ca>
2023-06-19 10:43:04 -0400

    F04: Add path to watchlist tab

--- commerce/auctions/urls.py
+++ commerce/auctions/urls.py
@@ -4,10 +4,11 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("login", views.login_view, name="login"),
-    path("logout", views.logout_view, name="logout"),
-    path("register", views.register, name="register"),
-    path("create", views.createListing, name="create"),
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
+    path("register/", views.register, name="register"),
+    path("create/", views.createListing, name="create"),
     path("category/<str:category>", views.category_finder, name="category"),
-    path("listing/<int:id>", views.individual_listing, name="listing")
+    path("listing/<int:id>", views.individual_listing, name="listing"),
+    path("watchlist/", views.watchlist, name="watchlist")
 ]
